4f087901fcec840e647101b9912f67642c296377
Zainul-jamal <minsarshah1@gmail.com>
2023-10-08 12:30:28 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+//  var x = prompt("Where does the Pope live?");
+//  if (x === "karachi") {
+//  alert("Correct!");
+//  }
+//  if (x !== "Vatican") {
+//  alert("Wrong answer");
+//  }
+// var pizza= prompt("Enter your Order => chiken")
+// if(pizza ==='chiken')
+//  { document.write('chiken pizza ')
+    
+//     var size=prompt('Enter pizza size => large')
+//     if(size ==='large'){
+//         document.write(' large')
+//     }else{document.write('Please chose the correct answer')}
+// }else{document.write('uncorrect')}
